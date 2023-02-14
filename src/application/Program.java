4d9b255dbba1c8a8190e89a09b92829b16126eb7
Raissa Feitosa <raissa.f.ferreira01@gmail.com>
2023-02-14 16:45:44 -0300

    fizemos o tabuleiro e mexemos na peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,12 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
-		
-		System.out.println("ola mundinho, pfvr f");
-
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
-
 }
