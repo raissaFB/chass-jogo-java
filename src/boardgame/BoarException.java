33646219f4762df24ca960e3b1e714eace1126c2
Raissa Feitosa <raissa.f.ferreira01@gmail.com>
2023-02-24 15:44:39 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoarException.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class BoarException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public BoarException(String msg) {
+		super(msg);
+	}
+}
